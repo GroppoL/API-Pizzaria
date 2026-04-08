7b1c2033e3b1bffbe85b0db93108a224a01cf8e1
Groppo <livrinhagr@gmail.com>
2026-04-08 16:34:46 -0300

    Adição da tabela e inserção das bebidas

--- config/bd.sql
+++ config/bd.sql
@@ -13,3 +13,19 @@ INSERT INTO pizzas (nome, ingredientes, valor) VALUES
 ('Moda do Juca', 'Mussarela, peito de peru, palmito, alho poró e alcaparras', 72.50);
  
 SELECT * FROM pizzas
+
+
+CREATE TABLE bebidas (
+    idBebida INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    tipo VARCHAR(50) NOT NULL,
+    qtdLitros VARCHAR(50) NOT NULL,
+    valor DECIMAL(10, 2) NOT NULL
+);
+INSERT INTO bebidas (nome, tipo, qtdLitros, valor) VALUES
+('Pepsi black', 'zero','500ml', 3.50),
+('Schweppes', 'tradicional','550ml', 4.00),
+('Coca-cola', 'zero','2L', 8.00),
+('Sukita sabor laranja', 'tradicional', '2,5L', 7.50),
+('Guarana antartica', 'zero', '2L',7.50 );
+SELECT * FROM bebidas

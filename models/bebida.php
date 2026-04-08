d2f15018fd925f299e19f75cdbbeb06d0d4aa6da
Groppo <livrinhagr@gmail.com>
2026-04-08 16:34:54 -0300

    Classe criada

---
+++ models/bebida.php
@@ -0,0 +1,28 @@
+<?php
+
+class Bebida{
+
+    private $conn;
+    private $tabela ="bebidas";
+    public $idBebida;
+    public $nome;
+    public $tipo;
+    public $qtdLitros;
+    public $valor;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+    function read() {
+        $query = "SELECT idBebida, nome, tipo, qtdLitros, valor FROM " . $this->tabela . " ORDER BY valor";
+ 
+        // Prepara a query
+        $stmt = $this->conn->prepare($query);
+ 
+        // Executa a query
+        $stmt->execute();
+ 
+        return $stmt;
+    }
+}
+
